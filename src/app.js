897fe3ce44d00ea9ff661253d3ebf83a5b133d84
nain333 <hnain70@gmai.com>
2026-07-04 23:56:04 +0530

    feat(auth): add JWT authentication middleware

--- src/app.js
+++ src/app.js
@@ -3,9 +3,10 @@ import userRouter from "./features/user/routes/user.routes.js"
 import productRouter from './features/product/routes/product.routes.js';
 import basicAuthorizer from "./middlewares/basic.auth.middleware.js";
 import bodyParser from "body-parser";
+import jwtAuth from "../src/middlewares/jwt.middleware.js"
 const app = express();
 app.use(bodyParser.json())
-app.use('/api/products',basicAuthorizer,productRouter);
+app.use('/api/products',jwtAuth,productRouter);
 app.use("/api/users",userRouter)
 
 export default app;
---
+++ src/middlewares/jwt.middleware.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken"
+const jwtAuth=(req,res,next)=>{
+    // 1. read the token
+    const token = req.headers["authorization"];
+    
+    
+    // 2.if not token,return the error
+    if(!token){
+        return res.status(401).send("Unathorized")
+    }
+    // 3.check if token is valid
+    try{ const payload = jwt.verify(
+        token,
+        process.env.JWT_SECRET
+    );
+    console.log(payload)
+        
+    }
+    // 4. return error
+    catch(err){
+        return res.status(401).send("Unathorized");
+    }
+    // 5. call next middleware
+    next();
+
+    
+}
+export default jwtAuth;

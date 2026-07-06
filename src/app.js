c51ab66d3388a7402dcd20ac19cc62613cd4df7f
nain333 <hnain70@gmai.com>
2026-07-06 15:49:10 +0530

    docs: enhance OpenAPI documentation with examples and authentication guidance

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 import express from "express";
+import swagger from "swagger-ui-express"
 import userRouter from "./features/user/routes/user.routes.js"
 import productRouter from './features/product/routes/product.routes.js';
 import basicAuthorizer from "./middlewares/basic.auth.middleware.js";
 import bodyParser from "body-parser";
 import jwtAuth from "../src/middlewares/jwt.middleware.js"
 import cartRouter from "./features/cartItems/routes/cartItems.routes.js";
+
+import apiDocs from "../swagger.json" with{type:'json'}
 const app = express();
 app.use(bodyParser.json())
+app.use(
+  "/api-docs",
+  swagger.serve,
+  swagger.setup(apiDocs)
+);
 app.use('/api/products',jwtAuth,productRouter);
 app.use("/api/cartItems",cartRouter)
 app.use("/api/users",userRouter)

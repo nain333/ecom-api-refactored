b16b4bcc6f76825441bbf8eda332a260de965da3
nain333 <hnain70@gmai.com>
2026-07-06 17:19:24 +0530

    feat: configure CORS middleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import swagger from "swagger-ui-express"
+import cors from "cors";
 import userRouter from "./features/user/routes/user.routes.js"
 import productRouter from './features/product/routes/product.routes.js';
 import basicAuthorizer from "./middlewares/basic.auth.middleware.js";
@@ -9,6 +10,10 @@ import cartRouter from "./features/cartItems/routes/cartItems.routes.js";
 
 import apiDocs from "../swagger.json" with{type:'json'}
 const app = express();
+// CORS policy configuration
+app.use(cors())
+
+
 app.use(bodyParser.json())
 app.use(
   "/api-docs",
@@ -18,6 +23,9 @@ app.use(
 app.use('/api/products',jwtAuth,productRouter);
 app.use("/api/cartItems",cartRouter)
 app.use("/api/users",userRouter)
+app.use((req,res)=>{
+    res.status(404).send("API not found")
+} )
 
 export default app;
 
